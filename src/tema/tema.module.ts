21914793144261941b015ac1af916f949c521060
Gabriela Lima <gabriela.deabreulima@gmail.com>
2025-12-11 11:26:17 -0300

    feat(auth): implementa autenticação JWT com strategy, service e constantes

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tema } from "./entities/tema.entity";
-import { TemaController } from "../postagem/controllers/tema.controller";
+import { TemaController } from "../tema/controllers/tema.controller";
 import { TemaService } from "./services/tema.service";
 
  
